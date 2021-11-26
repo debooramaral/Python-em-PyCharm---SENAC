675c882fb45d59dfc8ad20ab1e9dfaa181f9111d
Débora Amaral <87581131+debooramaral@users.noreply.github.com>
2021-11-26 15:27:47 -0400

    Add files via upload
    SÃO DOIS CHIPS DE ALIMENTAÇÃO

--- listaN4.py
+++ listaN4.py
@@ -1,7 +1,7 @@
 print('TEC FRANGO\n')
 frango = int(input('Quantos frangos são '))
 chipID = 4
-chipAL = 3.5
+chipAL = 2*3.5
 gastoFrango = chipID + chipAL
 granja = frango * gastoFrango
 print('o gasto total da sua granja é de: ', granja)
